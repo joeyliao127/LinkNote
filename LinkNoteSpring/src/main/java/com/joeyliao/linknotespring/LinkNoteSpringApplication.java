73cebfbec8f28bd796f20704eaf304594288b89f
JoeyLiao <joey01271998@gmail.com>
2023-11-01 13:59:34 +0800

    first commit

---
+++ LinkNoteSpring/src/main/java/com/joeyliao/linknotespring/LinkNoteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.joeyliao.linknotespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkNoteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkNoteSpringApplication.class, args);
+	}
+
+}
---
+++ LinkNoteSpring/src/test/java/com/joeyliao/linknotespring/LinkNoteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeyliao.linknotespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkNoteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
