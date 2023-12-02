35f78f18912dca641812488f13b2ff861273b15b
JoeyLiao <joey01271998@gmail.com>
2023-12-02 14:43:30 +0800

    js: show all notebook's note

--- LinkNoteSpring/src/main/resources/static/userSpace/js/userSpaceSidebar.js
+++ LinkNoteSpring/src/main/resources/static/userSpace/js/userSpaceSidebar.js
@@ -99,14 +99,13 @@ function notebooksBtnListener(isCoNotebook) {
   let selectedId = -1;
 
   for (let i = 0; i < notebooks.length; i++) {
-    notebooks[i].addEventListener("click", () => {
+    notebooks[i].addEventListener("click", async () => {
       if (selectedId == -1) {
         notebooks[i].classList.toggle("selected");
       } else {
         notebooks[selectedId].classList.toggle("selected");
         notebooks[i].classList.toggle("selected");
       }
-      console.log(notebooks[i]);
       setNotes(notebooks[i]);
       selectedId = i;
     });
@@ -115,22 +114,55 @@ function notebooksBtnListener(isCoNotebook) {
 
 let notesDataMap = {};
 //傳入notebook物件，取得notebookId & name & description
-function setNotes(notebook) {
+async function setNotes(notebook) {
   const notebookName = notebook.dataset.name;
-  console.log(!notesDataMap.hasOwnProperty(notebookName));
   if (!notesDataMap.hasOwnProperty(notebookName)) {
     notesDataMap[notebookName] = {};
     notesDataMap[`${notebookName}`]["name"] = notebookName;
     notesDataMap[`${notebookName}`]["description"] =
       notebook.dataset.description;
     notesDataMap[`${notebookName}`]["notebookId"] = notebook.dataset.notebookId;
+    const path = `/api/notebooks/${notebook.dataset.notebookId}/notes?offset=0&limit=20`;
+    const result = await fetchData(path, "GET");
+    notesDataMap[notebookName]["notes"] = result.notes;
+    console.log(notesDataMap);
   }
   const notebookNameCtn = document.querySelector(".main-group-subjectInfo h2");
   const descriptionCtn = document.querySelector(".main-group-subjectInfo p");
   notebookNameCtn.dataset.notebookId = notesDataMap[notebookName].notebookId;
   notebookNameCtn.textContent = notesDataMap[notebookName].name;
   descriptionCtn.textContent = notesDataMap[notebookName].description;
+  const noteCardCtn = document.querySelector(".main-group-notes");
+  noteCardCtn.innerHTML = "";
+  notesDataMap[notebookName].notes.forEach((note) => {
+    console.log(`創建子物件`);
+    const noteCard = document.createElement("div");
+    const star = document.createElement("img");
+    const noteName = document.createElement("h3");
+    const qeustion = document.createElement("p");
+    const createDate = document.createElement("p");
+    noteCard.classList.add("main-item-noteCard");
+    noteCard.classList.add("flex");
+    createDate.classList.add("date");
+    if (note.star) {
+      star.src = "/static/resource/images/star-full.png";
+    } else {
+      star.src = "/static/resource/images/star-empty.png";
+    }
+
+    star.alt = "star";
+    star.classList.add("btn");
+    noteName.textContent = note.name;
+    qeustion.textContent = note.qeustion;
+    createDate.textContent = note.createDate.split(" ")[0];
+    noteCard.appendChild(star);
+    noteCard.appendChild(noteName);
+    noteCard.appendChild(qeustion);
+    noteCard.appendChild(createDate);
+    noteCardCtn.appendChild(noteCard);
+  });
 }
+
 //取得notebook -> 觀察notebook -> fetch notebook -> 觀察notebook
 
 sideBarInit();
