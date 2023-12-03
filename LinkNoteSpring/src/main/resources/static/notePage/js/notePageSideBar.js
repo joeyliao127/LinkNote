cb6b5e68104c9759efaaf99e6a6ce3f793dea91f
JoeyLiao <joey01271998@gmail.com>
2023-12-03 13:39:44 +0800

    js: RESTful URL & note selected

--- LinkNoteSpring/src/main/resources/static/notePage/js/notePageSideBar.js
+++ LinkNoteSpring/src/main/resources/static/notePage/js/notePageSideBar.js
@@ -13,15 +13,20 @@ async function setAllNoteBtn() {
   const path = `/api/notebooks/${notebookId}/notes?limit=20`;
   //noteData is iterable
   const noteData = await fetchData(path, "GET");
-  console.log(noteData);
+  const notesGroup = document.querySelector(".notes-group");
   noteData.notes.forEach((note) => {
-    createNoteTitle(note.star, note.name, note.select, note.noteId);
-    //已經製作玩noteBtn，接著要監聽
+    const noteItem = createNoteTitle(
+      note.star,
+      note.name,
+      note.select,
+      note.noteId
+    );
+    notesGroup.appendChild(noteItem);
+    addNoteBtnListener(noteItem);
   });
 }
 
 function createNoteTitle(star, name, select, noteId) {
-  const notesGroup = document.querySelector(".notes-group");
   const noteItem = document.createElement("div");
   const noteTitle = document.createElement("p");
   noteItem.classList.add("note-item");
@@ -37,13 +42,28 @@ function createNoteTitle(star, name, select, noteId) {
     img.setAttribute("src", "/static/resource/images/star-full.png");
     noteItem.appendChild(img);
   }
-  addNoteBtnListener(noteItem);
-  notesGroup.appendChild(noteItem);
+  return noteItem;
 }
 
+let flag = -1;
+
+function removehHighlightNoteBtn() {
+  const noteList = document.querySelectorAll(".note-item");
+  console.log(`取得所有note`);
+  console.log(noteList);
+  console.log(`取消的noteId = ${flag}`);
+  noteList.forEach((item) => {
+    if (item.dataset.noteId === flag) {
+      item.classList.toggle("selected");
+    }
+  });
+}
 //傳遞note物件
 function addNoteBtnListener(note) {
   note.addEventListener("click", async () => {
+    removehHighlightNoteBtn();
+    note.classList.toggle("selected");
+    flag = note.dataset.noteId;
     const noteId = note.dataset.noteId;
     const path = `/api/notebooks/${notebookId}/notes/${noteId}`;
     console.log(`path = ${path}`);
@@ -64,14 +84,17 @@ function addNoteBtnListener(note) {
       date.textContent = data.createDate.split(" ")[0];
       if (data.star) {
         starBtn.setAttribute("src", "/static/resource/images/star-full.png");
-      } else {
-        starBtn.setAttribute("src", "/static/resource/images/star-empty.png");
       }
       if (data.shared) {
         lockBtn.setAttribute("src", "/static/resource/images/lock.png");
       } else {
         lockBtn.setAttribute("src", "/static/resource/images/unlock.png");
       }
+      history.pushState(
+        { noteId },
+        "",
+        `/notebooks/${notebookId}/notes/${noteId}`
+      );
     } else {
       MsgMaker.error("error..");
       return;
@@ -84,7 +107,12 @@ function createNoteBtnListener() {
   createNoteBtn.addEventListener("click", async () => {
     const path = `/api/notebooks/${notebookId}/notes`;
     const result = await fetchData(path, "POST");
-    createNoteTitle(false, "new note", true, result.noteId);
+    const notesGroup = document.querySelector(".notes-group");
+    removehHighlightNoteBtn();
+    const noteItem = createNoteTitle(false, "new note", false, result.noteId);
+    flag = result.noteId;
+    notesGroup.appendChild(noteItem);
+    addNoteBtnListener(noteItem);
   });
 }
 
